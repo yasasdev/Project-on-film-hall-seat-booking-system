9d4e3360714a7e37c9e4d7d5dbd633b98302406b
Yasas Lekamge <95513820+yasasdev@users.noreply.github.com>
2024-10-19 01:28:10 +0530

    Delete EAD 2 Project directory

--- EAD 2 Project/src/main/java/example/question/bill_calculator/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package example.question.bill_calculator.repository;
-
-import example.question.bill_calculator.Model.BookingModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<BookingModel, Long> {
-
-}
